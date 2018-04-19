4fa0bcb2842f4d89b95c785e70d0f1407911fef6
jeikerxiao <jeiker@126.com>
2018-04-19 11:15:05 +0800

    init project

---
+++ src/main/java/com/jeiker/wechat/SpringbootWechatApplication.java
@@ -0,0 +1,12 @@
+package com.jeiker.wechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWechatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWechatApplication.class, args);
+	}
+}
