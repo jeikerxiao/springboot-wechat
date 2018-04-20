356460dd9874f82772ebdd80082a8964405d0b9c
jeikerxiao <jeiker@126.com>
2018-04-20 15:44:10 +0800

    fix xml root element name

--- src/main/java/com/jeiker/wechat/model/vo/LinkMessageXml.java
+++ src/main/java/com/jeiker/wechat/model/vo/LinkMessageXml.java
@@ -2,9 +2,11 @@ package com.jeiker.wechat.model.vo;
 
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlCData;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import lombok.Data;
 
 @Data
+@JacksonXmlRootElement(localName = "xml")
 public class LinkMessageXml extends BaseMessageXml{
 
     /**
